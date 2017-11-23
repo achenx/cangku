59e65adf62f297016a97c1ade6406ccb5358f888
chenx <luckychenx@126.com>
2017-11-23 21:35:36 +0800

    Initial commit

---
+++ src/test/java/com/cx/test/GitTest.java
@@ -0,0 +1,12 @@
+package com.cx.test;
+
+/**
+ * Created by ASUS on 2017/11/23.
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        String str="asdfghjkl";
+            System.out.println(str.charAt(0));
+    }
+}
